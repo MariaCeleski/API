ad4e9f720c9154dff2ad5ebe08fc5480bf7896ad
Maria de Lourdes Celeski <marialceleski@gmail.com>
2022-04-12 12:37:27 -0300

    PUT

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const res = require("express/lib/response");
 
 const app = express();
 
 app.listen("3000");
 //usar insomnia
-//middleware
-app.use(express.json());
-app.route("/").post((req, res) => res.send(req.body));
+
+//MÉTODO POST
+//middleware = app.use(express.json());
+
+//app.use(express.json());
+//app.route("/").post((req, res) => res.send(req.body));
+
+//MÉTODO PUT
+
+app.use(express.json()); //middleware
+
+let author = "Maria";
+
+app.route("/").get((req, res) => res.send(author));
+
+app.route("/").put((req, res) => {
+  author = req.body.author;
+  res.send(author);
+});
